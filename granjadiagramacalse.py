39cf26cd6099fbd17824f9240a7bc019ff2c089b
andresfelipe-ui <andresfeliperodriguezprimicier@gmail.com>
2025-06-19 20:13:12 -0500

    Add files via upload

---
+++ granjadiagramacalse.py
@@ -0,0 +1,12 @@
+class Granja:
+    def __init__(self, nombre):
+        self.nombre: str = nombre
+        self.empleados = []
+
+    def administrar(self):
+        print("Administrando granja...")
+
+    def generar_reportes(self):
+        print("Generando reportes de la granja...")
+
+
